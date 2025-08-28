0e44221314c45e736f9afc1da1262ed383d34509
ali <alihasansaad1@gmail.com>
2025-08-28 11:36:16 +0300

    import render and created base.html and about.html that extends from base.html and modified the views page to render base and home.html i will still create home.html later

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Ultras World</h1>')
-
+    return render(request,'home.html')
 def about(request):
-    return HttpResponse('<h1>About Ultras World</h1>')
+    return render(request,'about.html')
